779c26505c2453bc2a335e9e1cdaf20b3f16db3f
nataliey16 <102062208+nataliey16@users.noreply.github.com>
2022-10-06 21:30:27 -0600

    Added the ability to show the definitions of searched words

--- dictionary-app/src/Dictionary.js
+++ dictionary-app/src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState("");
 
   function handleSearchResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function handleSearchSubmit(event) {
@@ -38,6 +41,7 @@ export default function Dictionary() {
           ></input>
         </div>
       </form>
+      <Results results={results} />
     </div>
   );
 }
